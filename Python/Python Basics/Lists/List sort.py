08acc500df43d0171489b6fbdc0624b481282c58
Pramod1324 <76099629+Pramod1324@users.noreply.github.com>
2026-05-21 23:43:11 +0530

    Practice
    test

---
+++ Python/Python Basics/Lists/List sort.py
@@ -0,0 +1,6 @@
+a = [0,100,1,200,577,234,98,76,33]
+for i in range(len(a)):
+    for j in range(len(a)-1):
+        if a[i]<a[j]:
+            a[i],a[j] = a[j],a[i]
+print(a)
---
+++ Python/Python Basics/Strings/Reverse string.py
@@ -0,0 +1,5 @@
+a = 'pramod'
+b = ''
+for i in a:
+    b = i + b
+print(b)
